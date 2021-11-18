de27698781d8ab1d429394a6dbf59d7d5c4b4de6
mbarrientos1129 <91514897+mbarrientos1129@users.noreply.github.com>
2021-11-17 20:19:14 -0500

    Add files via upload

---
+++ Module-6/javascript.js
@@ -0,0 +1,19 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.width = "500px";
+    document.getElementById("box").style.height = "500px";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor = "blue";
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+    document.getElementById("box").style.opacity = "0.25";
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+});
